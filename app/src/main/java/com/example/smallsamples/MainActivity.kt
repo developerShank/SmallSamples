ecea0d5d79d8fcd0ff0db8e6a73ef83aba836a74
ShashankMohan <Shashank.MohanUnnithan_1@philips.com>
2022-08-20 17:24:50 +0530

    architectural component live data

--- app/src/main/java/com/example/smallsamples/MainActivity.kt
+++ app/src/main/java/com/example/smallsamples/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.smallsamples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    val tvInitial: TextView
+        get() = findViewById(R.id.tv_initial)
+
+    val btnUpdate: Button
+        get() = findViewById(R.id.btn_update)
+
+    lateinit var mainActivityViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainActivityViewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
+         mainActivityViewModel.liveData.observe(this) {
+             tvInitial.text = it
+         }
+        btnUpdate.setOnClickListener {
+            mainActivityViewModel.updateFacts()
+        }
     }
+
 }

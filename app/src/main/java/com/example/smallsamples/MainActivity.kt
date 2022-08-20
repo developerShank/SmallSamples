7814c75d372d906d3559cec7a32d1333fef01dfc
ShashankMohan <Shashank.MohanUnnithan_1@philips.com>
2022-08-20 17:46:10 +0530

    architectural component Data binding

--- app/src/main/java/com/example/smallsamples/MainActivity.kt
+++ app/src/main/java/com/example/smallsamples/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.smallsamples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.smallsamples.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val quote = Quote("Do, or dont do, there is not try", "Yoda")
+        activityMainBinding.quote = quote
 
     }
 }
---
+++ app/src/main/java/com/example/smallsamples/Quote.kt
@@ -0,0 +1,7 @@
+package com.example.smallsamples
+
+/**
+ * (C) Koninklijke Philips N.V., 2022.
+ * All rights reserved.
+ */
+data class Quote(val quote: String, val author: String)
